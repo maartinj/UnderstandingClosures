559caadb4e3e78740cebf88f96d25bdebbc6d5c3
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2022-10-23 03:46:37 +0200

    Add files via upload

---
+++ UnderstandingClosures.playground/Sources/PublicFunctions.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+public func code(for title: String,
+                    sampleCode: () -> Void) {
+print("""
+_____________________________
+Results for: '\(title)'
+""")
+  sampleCode()
+}
+
+
